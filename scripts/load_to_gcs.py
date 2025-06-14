c7cbc66deb9b0784d5b9f13fd82db8965f0934ca
lattalavijaysekhar <167703985+lattalavijaysekhar@users.noreply.github.com>
2025-06-14 23:37:05 +0800

    Update load_to_gcs.py
    Updated the GCS bucket details

--- scripts/load_to_gcs.py
+++ scripts/load_to_gcs.py
@@ -9,4 +9,4 @@ def upload_to_gcs(bucket_name, source_file_path, destination_blob_path):
     blob.upload_from_filename(source_file_path)
     print(f"Uploaded {source_file_path} to gs://{bucket_name}/{destination_blob_path}")
 
-upload_to_gcs('heymax-raw', 'data/sample_events.csv', 'events/sample_events.csv')
+upload_to_gcs('heymax-raw', 'data/event_stream.csv', 'event_stream.csv')
