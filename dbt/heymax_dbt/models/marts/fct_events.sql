c430a8beb0cc473c41856695c5fa064ecc73832a
lattalavijaysekhar <167703985+lattalavijaysekhar@users.noreply.github.com>
2025-06-15 15:51:59 +0800

    Update fct_events.sql
    Updating the query

--- dbt/heymax_dbt/models/marts/fct_events.sql
+++ dbt/heymax_dbt/models/marts/fct_events.sql
@@ -1,10 +1,20 @@
 {{ config(
     materialized='incremental',
-    unique_key='user_id||event_time||event_type'
+    unique_key='user_id||event_date||event_type'
 ) }}
 
-SELECT * FROM `heymax-analytics.heymax_staging.stg_events`
+SELECT
+  user_id,
+  event_type,
+  event_time,
+  DATE(event_time) AS event_date,
+  transaction_category,
+  miles_amount,
+  platform,
+  utm_source,
+  country
+FROM `heymax-analytics.heymax_staging.stg_events`
 
 {% if is_incremental() %}
-  WHERE event_time > (SELECT MAX(event_time) FROM {{ this }})
+WHERE DATE(event_time) > (SELECT MAX(event_date) FROM {{ this }})
 {% endif %}
